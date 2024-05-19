3cb7fd6bd81c9d41be1479cfc3e743544b4c4f07
Akash Tiwari <akashtiwari722003@gmail.com>
2024-05-19 18:08:00 +0530

    registration_basic

---
+++ Backend/employee_management_portal/src/main/java/com/akash/employee_management_portal/dto/RegistrationRequest.java
@@ -0,0 +1,50 @@
+package com.akash.employee_management_portal.dto;
+
+public class RegistrationRequest {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String type;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
--- Backend/employee_management_portal/src/main/java/com/akash/employee_management_portal/service/UserServiceImpl.java
+++ Backend/employee_management_portal/src/main/java/com/akash/employee_management_portal/service/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.akash.employee_management_portal.service;
 
+import com.akash.employee_management_portal.dto.RegistrationRequest;
 import com.akash.employee_management_portal.entity.User;
+import com.akash.employee_management_portal.entity.Worker;
 import com.akash.employee_management_portal.repository.UserRepository;
+import com.akash.employee_management_portal.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +15,23 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService{
 
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private WorkerRepository workerRepository;
+
+
+
+//    @Autowired
+//    public UserServiceImpl(WorkerRepository workerRepository) {
+//        this.workerRepository = workerRepository;
+//    }
+
+    //    @Autowired
+//    public UserServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
 
 
     @Override
@@ -35,4 +49,17 @@ public class UserServiceImpl implements UserService{
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
+
+    @Override
+    public ResponseEntity<String> registerUser(RegistrationRequest registrationRequest) {
+        Worker worker = new Worker(registrationRequest.getEmail(),
+                registrationRequest.getFirstName(),
+                registrationRequest.getLastName(),
+                registrationRequest.getPassword(),
+                registrationRequest.getType());
+
+        workerRepository.save(worker);
+        return ResponseEntity.ok("User Registered Successfully");
+    }
+
 }
