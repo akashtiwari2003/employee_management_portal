b813740f5aabc70295063c24e42b74c5ab922b33
Akash Tiwari <akashtiwari722003@gmail.com>
2024-06-04 15:15:04 +0530

    admin_dashboard_basic

--- Frontend/adminDashboard.js
+++ Frontend/adminDashboard.js
@@ -6,3 +6,28 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("welcomeMessage").innerText = "Welcome, Guest!";
     }
 });
+
+const addBtn = document.getElementById("addUser");
+addBtn.addEventListener("click", function() {
+    window.location.href = "addEmployeeManager.html";
+});
+
+const viewUsersBtn = document.getElementById("viewUsersBtn");
+const userCardsContainer = document.querySelector(".user-cards");
+
+viewUsersBtn.addEventListener("click", function() {
+  fetch("http://localhost:8085/all")
+    .then(response => response.json())
+    .then(data => {
+      userCardsContainer.innerHTML = "";
+      data.forEach(user => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `<h2>${user.email}</h2>`;
+        card.innerHTML = ``;
+        userCardsContainer.appendChild(card);
+      });
+    })
+    .catch(error => console.error(error));
+});
+
